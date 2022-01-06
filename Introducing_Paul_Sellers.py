b30bee421c4ba4a19989ba2d045183506cc695b0
Evanchi7 <97007717+Evanchi7@users.noreply.github.com>
2022-01-05 22:27:48 -0500

    Add files via upload

---
+++ Introducing_Paul_Sellers.py
@@ -0,0 +1,5 @@
+name = input("What is your full name?: ")
+profession = input("What is your profession?: ")
+years = input(f"How many years have you been a {profession} for?: ")
+introduction = f"Everyone, this is {name} and he has been a {profession} for {years} years."
+print(introduction)
